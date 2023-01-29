7c9a46f387afdcf435e7330351f42e30ccba01bd
nemanjamilosevic98 <nemanjamilosevic70@gmail.com>
2023-01-29 16:27:36 +0100

    Nemanja - shared/models

---
+++ src/app/shared/models/address.model.ts
@@ -0,0 +1,14 @@
+export interface Address {
+  city: string,
+  cityCode: string,
+  countryName: string,
+  county: string,
+  countyCode: string,
+  district: string,
+  houseNumber: string,
+  label: string,
+  postalCode: string,
+  state: string,
+  stateCode: string,
+  street: string
+}
